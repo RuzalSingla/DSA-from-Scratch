c76c2b82a996c4946b451223eef73b2074fe525c
RuzalSingla <ruzalsingla2301@gmail.com>
2025-08-25 20:34:15 +0530

    Selection Sort: Sorting

--- Sorting Techniques/selection_sort.cpp
+++ Sorting Techniques/selection_sort.cpp
@@ -2,7 +2,32 @@
 using namespace std;
 
 int main(){
+    int n;
+    cout<<"Enter number of elements in the array: ";
+    cin>>n;
 
+    int arr[n];
+    for(int i=0 ; i<n ; i++){
+        cin>>arr[i];
+    }
+
+    for(int i=0 ; i<n-1 ; i++){
+        int min=i;
+        for(int j=i+1 ; j<n ; j++){
+            if(arr[j]<arr[min]){
+                min=j;
+            }
+        
+        }
+        swap(arr[i],arr[min]);
+        
+    }
+
+    cout<<"The sorted list using selection sort is as follows :"<<endl;
+
+    for(int i=0 ; i<n ; i++){
+        cout<<arr[i]<<" , ";
+    }
 
     return 0;
 }
