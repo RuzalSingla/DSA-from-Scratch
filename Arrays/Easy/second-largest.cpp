71598012ba38fb7e304e980e83daf424f61850ed
RuzalSingla <ruzalsingla2301@gmail.com>
2025-08-27 10:21:52 +0530

    Second Largest : Array

---
+++ Arrays/Easy/second-largest.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter n:";
+    cin>>n;
+    int arr[n];
+
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    int largest=arr[0];
+    int second_largest=INT32_MIN;
+    
+    for(int j=0 ; j<n ;j++){
+        if(arr[j]>largest){
+            second_largest=largest;
+            largest=arr[j];
+        }
+
+        if(arr[j]<largest && arr[j]>second_largest){
+            second_largest=arr[j];
+        }
+    }
+
+    cout<<"Second largest is :"<<second_largest;
+    
+
+    return 0;
+}
